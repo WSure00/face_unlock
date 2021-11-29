fc72471d01cf12597219911b8f18c6c74545c5e5
wangshuo16 <wangshuo16@xiaomi.com>
2021-11-29 20:11:33 +0800

    new method

--- face_cam.py
+++ face_cam.py
@@ -8,23 +8,27 @@ import numpy as np
 from pathlib import Path
 from pykeyboard import PyKeyboard
 
-# import  psutil
-# for proc in psutil.process_iter():
-#     if (proc.name()=='python'):
-#         os.system("sudo renice 1 %s "%(proc.pid))
+import  psutil
+for proc in psutil.process_iter():
+    if (proc.name()=='python'):
+        os.system("sudo renice 1 %s "%(proc.pid))
 
 
 myface= Path("myface.npy")
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(1)
+n=0
+queue=[]
+verify=4
+faceon=True
 
 def revert(img):
-    image=cv2.transpose(img)
-    new_img = cv2.flip(image, -1)
+    # image=cv2.transpose(img)          #图像转置
+    new_img = cv2.flip(img, 0)         #图像横置
     return new_img
 
 def unlock(i):
     global queue
-
+    global faceon
     flag=True
     if len(queue)>=i:
         for index in range(i):
@@ -36,9 +40,12 @@ def unlock(i):
             if  os.system('gnome-screensaver-command -q | grep in') :
                 k=PyKeyboard()
                 k.tap_key(k.enter_key)
+                os.system('gnome-screensaver-command -d')
+                faceon=True
             
         else :
-            os.system('gnome-screensaver-command -a')
+            faceon=False
+            os.system('gnome-screensaver-command -l')
         return flag
 
 if myface.exists():
@@ -46,43 +53,43 @@ if myface.exists():
     # print("my_face_encoding: ",time.clock()-start)
 else:
     picture_of_me = face_recognition.load_image_file("me.jpg")
-    my_face_encoding = face_recognition.face_encodings(picture_of_me,num_jitters=2)[0]
+    my_face_encoding = face_recognition.face_encodings(picture_of_me,num_jitters=2,model='large')[0]
     # print("my_face_encoding: ",time.clock()-start)
     np.save("myface.npy",my_face_encoding)
-
-n=0
-queue=[]
-verify=3
 
 while True:
     # start=time.clock()
     success,img=camera.read()
-    # frame = cv2.resize(img, (0, 0), fx=0.25, fy=0.25)
+    img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
     img_new=revert(img)
     marks = face_recognition.face_locations(img_new)
-    
     if len(marks)!=0:
-        flag=False
-        # codings = face_recognition.face_encodings(img_new, known_face_locations=marks)
-        # for coding in codings:
-        coding = face_recognition.face_encodings(img_new,num_jitters=2)[0]
-        result = face_recognition.compare_faces([my_face_encoding],coding,tolerance=0.6)
-        flag=result[0]
-        queue.append(flag)
-        n+=1
-        # print(queue)
-        unlock(verify)
-        print("flag: ",flag,n)
+        if faceon :
+            faceon=True
+            print("sleep")
+            time.sleep(1)
+        
+        else:
+            flag=False
+            # codings = face_recognition.face_encodings(img_new, known_face_locations=marks)
+            # for coding in codings:
+            coding = face_recognition.face_encodings(img_new,num_jitters=2,model='large')[0]
+            result = face_recognition.compare_faces([my_face_encoding],coding,tolerance=0.4)
+            flag=result[0]
+            queue.append(flag)
+            n+=1
+            # print(queue)
+            unlock(verify)
+            print("flag: ",flag,n)
         
             
             # if result :
             #     os.popen('gnome-screensaver-command -d && xdotool key Return')
     else :
-        os.system('gnome-screensaver-command -a')
+        faceon=False
+        os.system('gnome-screensaver-command -l')
     # cv2.imshow('face', img_new)
     # if cv2.waitKey(1) & 0xFF == ord('q'):
     #     break
-    
-    # print("\n",time.clock()-start)
 camera.release()
 # cv2.destroyAllWindows()
--- test.py
+++ test.py
@@ -1,7 +1,21 @@
-import os
-a=os.system('gnome-screensaver-command -q | grep in')
-print(a)
-if a:
-    print("true")
-else:
-    print("false")
+import cv2
+#引入库
+
+cap = cv2.VideoCapture(1)
+
+def revert(img):
+    # image=cv2.transpose(img)
+    image = cv2.flip(img, 0)
+    return image
+
+while True:
+    ret, frame = cap.read()
+    img=revert(frame)
+    cv2.imshow("Video", img)
+#读取内容
+    if cv2.waitKey(10) == ord("q"):
+        break
+        
+#随时准备按q退出
+cap.release()
+cv2.destroyAllWindows()
